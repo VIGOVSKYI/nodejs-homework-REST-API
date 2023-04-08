720b3131895eec2ece7f27b2897825fcab570971
VIGOVSKYI <vuv3000@ukr.net>
2023-04-08 12:15:54 +0300

    env

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
 
-const DB_HOST = "mongodb+srv://Urii:UTDglRm59UPLPNvp@cluster01.wuv69yw.mongodb.net/Contacts_data?retryWrites=true&w=majority"
+const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.connect(DB_HOST)
-  .then(() => app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-  }
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
   )
-  );
-
-
+  .catch((error) => console.log(error.mesage));
 
 // UTDglRm59UPLPNvp
 
