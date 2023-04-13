9b5fa3e7d94290430663b7d1906a637e0e49958e
VIGOVSKYI <vuv3000@ukr.net>
2023-04-13 14:09:41 +0300

    logout

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,55 +1,74 @@
-
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const { wrapperControlers } = require("../utils/index");
 
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 
 const { HttpError } = require("../helpers/index");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-const register = async(req, res)=> {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(user) {
-        throw HttpError(409, "Email in use");
-    }
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
-    
-    const result = await User.create({...req.body, password: hashPassword});
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    res.status(201).json({
-        name: result.name,
-        email: result.email,
-    })
-}
+  const result = await User.create({ ...req.body, password: hashPassword });
 
-const login = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user) {
-        throw HttpError(401, "Email or password is wrong");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password is wrong");
-    }
+  res.status(201).json({
+    name: result.name,
+    email: result.email,
+  });
+};
 
-    const payload = {
-        id: user._id,
-    }
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+  const payload = {
+    id: user._id,
+  };
 
-    res.json({
-        token,
-    })
-}
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({
+    token,
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { name, email } = req.user;
+
+  res.json({
+    name,
+    email,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "Logout success",
+  });
+};
 
 module.exports = {
-    register: wrapperControlers(register),
-    login: wrapperControlers(login),
-}
+  register: wrapperControlers(register),
+  login: wrapperControlers(login),
+  getCurrent: wrapperControlers(getCurrent),
+  logout: wrapperControlers(logout),
+};
--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -2,19 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const {validateBody} = require("../../utils");
+const { validateBody } = require("../../utils");
+
+const { authenticate } = require("../../middlewares/index");
 
 const { schemas } = require("../../models/user");
 const controllers = require("../../controllers/auth-controllers");
 
+router.post(
+  "/register",
+  validateBody(schemas.registerSchema),
+  controllers.register
+);
 
-router.post("/register", validateBody(schemas.registerSchema),)
+router.post("/login", validateBody(schemas.loginSchema), controllers.login);
 
-router.post("/login", validateBody(schemas.loginSchema), controllers.register);
+router.get("/current", authenticate, controllers.getCurrent);
 
+router.post("/logout", authenticate, controllers.logout);
 
 module.exports = router;
-
-
-
-
