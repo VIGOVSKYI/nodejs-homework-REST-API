c30772a47d114f4493cabbf0f7c6bdad62919917
VIGOVSKYI <vuv3000@ukr.net>
2023-04-13 13:17:56 +0300

    pagination

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -5,8 +5,10 @@ const { wrapperControlers } = require("../utils/index");
 const { Contact } = require("../models/contact");
 
 const getAllContacts = async (req, res) => {
-   const {_id: owner} = req.user;
-  const result = await Contact.find({owner}.populate("owner", "name email"));
+  const { _id: owner } = req.user;
+  const {page = 1, limit = 10} = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({owner},{skip, limit}).populate("owner", "name email");
   res.json(result);
 };
 
